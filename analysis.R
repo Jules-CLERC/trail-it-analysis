69afcfbd3ebad6ad25bec5b076ba9b345d0f1315
Bastian Ilso <hougaard.junior@gmail.com>
2021-09-23 09:29:28 +0200

    analysis.R: example of summarize, mutate, vis

--- analysis.R
+++ analysis.R
@@ -1,8 +1,12 @@
 library(RMySQL)
 library(tidyverse)
-library(ggplot2)
+library(plotly)
 
+#############
+# Load
+#############
 
+options("digits.secs"=6)
 credentials <- read.csv("credentials.csv", header=TRUE,sep=",", colClasses=c("character","character","character","character"))
 
 lapply( dbListConnections( dbDriver( drv = "MySQL")), dbDisconnect)
@@ -25,3 +29,22 @@ RetreiveDataSet <- function(tablename) {
 }
 
 D <- RetreiveDataSet(credentials[1, "table"])
+
+#############
+# Preprocess
+#############
+
+D = D %>% mutate(Timestamp = paste(date,time),
+                 Timestamp = as.POSIXct(Timestamp, format = "%Y-%m-%d %H:%M:%OS")) %>%
+          arrange(Timestamp)
+
+S = D %>% group_by(profileID) %>%
+  summarize(player_name = unique(playerName),
+            mean_best_time_sec = mean(sessionWorstReactionTime))
+
+#############
+# Visualize
+#############
+
+plot_ly() %>%
+  add_trace(data = S, x=~player_name, y=~mean_best_time_sec, type='bar', color=I('grey'))
